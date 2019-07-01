44a1e0c3cdac209ea0ce1aee3a4af6b7e0035328
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2019-07-01 09:46:09 -0300

    Update plotting.py

--- recogners/utils/plotting.py
+++ recogners/utils/plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-from PIL import Image
+import numpy as np
 import recogners.utils.wv as wv
+from PIL import Image
 
 def show(tensor):
     plt.figure()
@@ -9,8 +10,7 @@ def show(tensor):
     
 def weights_visualize(tensor):
     w8 = tensor.numpy()
-    d1 = w8.shape[0]
-    d2 = w8.shape[1]
+    d = int(np.sqrt(w8.shape[0]))
     s = int(np.sqrt(w8.shape[1]))
-    img = Image.fromarray(wv.tile_raster_images(X = w8.T, img_shape=(d1, d2), tile_shape=(s,s), tile_spacing=(1,1)))
+    img = Image.fromarray(wv.tile_raster_images(X = w8.T, img_shape=(d, d), tile_shape=(s,s), tile_spacing=(1,1)))
     img.show()
