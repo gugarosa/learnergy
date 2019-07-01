77dd78c54aa19cb6bba25f463d422a77a1548ced
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2019-07-01 09:31:04 -0300

    Added weights visualization

--- recogners/utils/plotting.py
+++ recogners/utils/plotting.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
+from PIL import Image
+
 
 def show(tensor):
     plt.figure()
     plt.imshow(tensor.numpy(), cmap=plt.cm.gray)
     plt.show()
+    
+def weights_visualize(tensor):
+    w8 = tensor.numpy()
+    d1 = w8.shape[0]
+    d2 = w8.shape[1]
+    s = int(np.sqrt(w8.shape[1]))
+    img = Image.fromarray(wv.tile_raster_images(X = w8.T, img_shape=(d1, d2), tile_shape=(s,s), tile_spacing=(1,1)))
+    img.show()
