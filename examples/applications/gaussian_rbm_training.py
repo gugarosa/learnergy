ae493a7e675cd10d56dbcff85969f72be59e6ac5
Gustavo Rosa <gth.rosa@uol.com.br>
2020-02-11 17:59:49 -0300

    Still need to proper address the Gaussian RBM.

--- examples/applications/gaussian_rbm_training.py
+++ examples/applications/gaussian_rbm_training.py
@@ -6,20 +6,26 @@ from learnergy.models.gaussian_rbm import GaussianRBM
 
 # Creating training and testing dataset
 train = torchvision.datasets.MNIST(
-    root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor())
+    root='./data', train=True, download=True, transform=torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.1307,), (0.3081,))
+    ]))
 test = torchvision.datasets.MNIST(
-    root='./data', train=False, download=True, transform=torchvision.transforms.ToTensor())
+    root='./data', train=False, download=True, transform=torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.1307,), (0.3081,))
+    ]))
 
 # Creating training and testing batches
 train_batches = DataLoader(train, batch_size=128, shuffle=True, num_workers=1)
 test_batches = DataLoader(test, batch_size=10000, shuffle=True, num_workers=1)
 
 # Creating a GaussianRBM
-model = GaussianRBM(n_visible=784, n_hidden=128, steps=1, learning_rate=0.1,
-                   momentum=0, decay=0, temperature=1, use_gpu=True)
+model = GaussianRBM(n_visible=784, n_hidden=128, steps=1, learning_rate=0.005,
+                    momentum=0, decay=0, temperature=1, use_gpu=True)
 
 # Training a GaussianRBM
-mse, pl = model.fit(train_batches, epochs=5)
+mse, pl = model.fit(train_batches, epochs=25)
 
 # Reconstructing test set
 rec_mse, v = model.reconstruct(test_batches)
