3ad6d5dc95057d8f5dee16b66868b13be1e01906
Gustavo Rosa <gth.rosa@uol.com.br>
2020-02-18 12:33:59 -0300

    Updating visual package script.

--- examples/visual/create_weights_mosaic.py
+++ examples/visual/create_weights_mosaic.py
@@ -1,5 +1,4 @@
 import torchvision
-from torch.utils.data import DataLoader
 
 import learnergy.visual.image as im
 from learnergy.models.rbm import RBM
@@ -8,15 +7,12 @@ from learnergy.models.rbm import RBM
 train = torchvision.datasets.MNIST(
     root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor())
 
-# Creating training batches
-train_batches = DataLoader(train, batch_size=128, shuffle=True, num_workers=1)
-
 # Creating an RBM
 model = RBM(n_visible=784, n_hidden=128, steps=1,
             learning_rate=0.1, momentum=0, decay=0, temperature=1)
 
 # Training an RBM
-model.fit(train_batches, epochs=5)
+model.fit(train, epochs=5)
 
 # Creating weights' mosaic
 im.create_mosaic(model.W)
