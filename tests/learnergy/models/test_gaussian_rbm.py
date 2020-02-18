15a3b3e0335a5b4896f236da585bb0d1a4ca82cb
Gustavo Rosa <gth.rosa@uol.com.br>
2020-02-18 15:22:34 -0300

    Just need to finish DBN tests.

---
+++ tests/learnergy/models/test_gaussian_rbm.py
@@ -0,0 +1,79 @@
+import pytest
+import torch
+
+from learnergy.models import gaussian_rbm
+
+
+def test_gaussian_rbm_visible_sampling():
+    new_gaussian_rbm = gaussian_rbm.GaussianRBM()
+
+    h = torch.ones(1, 128)
+
+    probs, states = new_gaussian_rbm.visible_sampling(h, scale=True)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+    probs, states = new_gaussian_rbm.visible_sampling(h, scale=False)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+
+def test_variance_gaussian_rbm_sigma():
+    new_variance_gaussian_rbm = gaussian_rbm.VarianceGaussianRBM()
+
+    assert new_variance_gaussian_rbm.sigma.size(0) == 128
+
+
+def test_variance_gaussian_rbm_sigma_setter():
+    new_variance_gaussian_rbm = gaussian_rbm.VarianceGaussianRBM()
+
+    try:
+        new_variance_gaussian_rbm.sigma = 1
+    except:
+        new_variance_gaussian_rbm.sigma = torch.nn.Parameter(torch.zeros(128))
+
+    assert new_variance_gaussian_rbm.sigma.size(0) == 128
+
+
+def test_variance_gaussian_rbm_hidden_sampling():
+    new_variance_gaussian_rbm = gaussian_rbm.VarianceGaussianRBM()
+
+    v = torch.ones(1, 128)
+
+    probs, states = new_variance_gaussian_rbm.hidden_sampling(v, scale=True)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+    probs, states = new_variance_gaussian_rbm.hidden_sampling(v, scale=False)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+
+def test_variance_gaussian_rbm_visible_sampling():
+    new_variance_gaussian_rbm = gaussian_rbm.VarianceGaussianRBM()
+
+    h = torch.ones(1, 128)
+
+    probs, states = new_variance_gaussian_rbm.visible_sampling(h, scale=True)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+    probs, states = new_variance_gaussian_rbm.visible_sampling(h, scale=False)
+
+    assert probs.size(1) == 128
+    assert states.size(1) == 128
+
+
+def test_variance_gaussian_rbm_energy():
+    new_variance_gaussian_rbm = gaussian_rbm.VarianceGaussianRBM()
+
+    samples = torch.ones(1, 128)
+
+    energy = new_variance_gaussian_rbm.energy(samples)
+
+    assert energy.detach().numpy() < 0
--- tests/learnergy/models/test_rbm.py
+++ tests/learnergy/models/test_rbm.py
@@ -303,7 +303,7 @@ def test_rbm_reconstruct():
         root='./data', train=False, download=True, transform=torchvision.transforms.ToTensor())
 
     new_rbm = rbm.RBM(n_visible=784, n_hidden=128, steps=1,
-                      learning_rate=0.1, momentum=0, decay=0, temperature=1)
+                      learning_rate=0.1, momentum=0, decay=0, temperature=1, use_gpu=False)
 
     e, v = new_rbm.reconstruct(test)
 
