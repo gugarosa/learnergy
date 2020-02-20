13c5304e0e9e73f8de613efc627a0b35128aabdd
Gustavo Rosa <gth.rosa@uol.com.br>
2020-02-20 13:51:01 -0300

    Fixing up tests for Travis.

--- tests/learnergy/visual/test_metrics.py
+++ tests/learnergy/visual/test_metrics.py
@@ -1,21 +1,24 @@
 import pytest
-import torch
 from learnergy.visual import metrics
 
 
 def test_metrics_plot():
-    new_model = torch.load('model.pth')
+    new_model = {
+        'mse': [1, 2, 3],
+        'pl': [1.5, 2, 2.5],
+        'time': [0.1, 0.2, 0.3]
+    }
 
     try:
         metrics.plot(
-            new_model.history['mse'], new_model.history['pl'], new_model.history['time'], labels=1)
+            new_model['mse'], new_model['pl'], new_model['time'], labels=1)
     except:
-        metrics.plot(new_model.history['mse'], new_model.history['pl'],
-                     new_model.history['time'], labels=['MSE', 'log-PL', 'time (s)'])
+        metrics.plot(new_model['mse'], new_model['pl'],
+                     new_model['time'], labels=['MSE', 'log-PL', 'time (s)'])
 
     try:
-        metrics.plot(new_model.history['mse'], new_model.history['pl'],
-                     new_model.history['time'], labels=['MSE'])
+        metrics.plot(new_model['mse'], new_model['pl'],
+                     new_model['time'], labels=['MSE'])
     except:
         metrics.plot(
-            new_model.history['mse'], new_model.history['pl'], new_model.history['time'])
+            new_model['mse'], new_model['pl'], new_model['time'])
