efaf33962d65f50556c3620fbe7b148f49e9054c
Gustavo Rosa <gth.rosa@uol.com.br>
2020-04-07 12:24:47 -0300

    Finishing up examples. Still need to test on CUDA.

---
+++ examples/models/create_discriminative_rbm.py
@@ -0,0 +1,5 @@
+from learnergy.models.discriminative_rbm import DiscriminativeRBM
+
+# Creates a DiscriminativeRBM-based class
+model = DiscriminativeRBM(n_visible=784, n_hidden=128, learning_rate=0.1,
+                          momentum=0, decay=0, use_gpu=False)
