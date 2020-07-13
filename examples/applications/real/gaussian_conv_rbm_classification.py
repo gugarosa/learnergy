5d49d33ee5c63d5365f8e49580c7bd0a004b1a0d
Gustavo Rosa <gth.rosa@uol.com.br>
2020-07-13 16:07:35 -0300

    Adding missing scripts and tests.

--- examples/applications/real/gaussian_conv_rbm_classification.py
+++ examples/applications/real/gaussian_conv_rbm_classification.py
@@ -1,54 +1,36 @@
-import os
-
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
-from torch import optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-import learnergy.visual.tensor as t
-#from learnergy.models.binary import ConvRBM
-from learnergy.models.real import ConvRBM
+from learnergy.models.real import GaussianConvRBM
 
-# Creating training and testing dataset
-train = torchvision.datasets.CIFAR10(
-	root='./data', train=True, download=True, transform=torchvision.transforms.Compose(
-	[#torchvision.transforms.Grayscale(num_output_channels=1), 
-	torchvision.transforms.ToTensor()]))
-test = torchvision.datasets.CIFAR10(
-	root='./data', train=False, download=True, transform=torchvision.transforms.Compose(
-	[#torchvision.transforms.Grayscale(num_output_channels=1), 
-	torchvision.transforms.ToTensor()]))
-
-n_classes = 10
-dim = 32
-if dim==32:
-    n_channels=3
-else:
-    n_channels=1
-
-j=0
-np.random.seed(j)
-torch.manual_seed(j)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-
-n_filters=16
+# Defining some input variables
+v_shape = 32
+n_filters = 16
 f_shape = 9
-batch_size=100
-# Creating a ConvRBM
-model = ConvRBM(visible_shape=(dim, dim), filter_shape=(f_shape, f_shape), n_filters=n_filters, n_channels=n_channels, learning_rate=0.00001, momentum=0.5, use_gpu=True)
+n_channels = 3
+batch_size = 100
+n_classes = 10
+fine_tune_epochs = 20
 
-# Training a ConvRBM
-mse = model.fit(train, batch_size=batch_size, epochs=1)
+# Creating training and validation/testing dataset
+train = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor())
+test = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=torchvision.transforms.ToTensor())
 
-torch.save(model, 'model.pth')
+# Creating a GaussianConvRBM
+model = GaussianConvRBM(visible_shape=(v_shape, v_shape), filter_shape=(f_shape, f_shape), n_filters=n_filters,
+                        n_channels=n_channels, learning_rate=0.00001, momentum=0.5, decay=0, use_gpu=True)
+
+# Training a GaussianConvRBM
+model.fit(train, batch_size=batch_size, epochs=5)
 
 # Creating the Fully Connected layer to append on top of RBM
-fc = torch.nn.Linear(model.hidden_shape[0]*model.hidden_shape[1]*n_filters, n_classes)
+fc = nn.Linear(model.hidden_shape[0] * model.hidden_shape[1] * n_filters, n_classes)
 
 # Check if model uses GPU
 if model.device == 'cuda':
@@ -63,11 +45,10 @@ optimizer = [optim.Adam(model.parameters(), lr=0.0001),
              optim.Adam(fc.parameters(), lr=0.001)]
 
 # Creating training and validation batches
-train_batch = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=1)
-val_batch = DataLoader(test, batch_size=10000, shuffle=True, num_workers=1)
+train_batch = DataLoader(train, batch_size=batch_size, shuffle=False, num_workers=1)
+val_batch = DataLoader(test, batch_size=10000, shuffle=False, num_workers=1)
 
-fine_tune_epochs = 5
-# For amount of fine-tuning epochs	
+# For amount of fine-tuning epochs
 for e in range(fine_tune_epochs):
     print(f'Epoch {e+1}/{fine_tune_epochs}')
 
@@ -79,7 +60,7 @@ for e in range(fine_tune_epochs):
         # For every possible optimizer
         for opt in optimizer:
             # Resets the optimizer
-            opt.zero_grad()        
+            opt.zero_grad()
 
         # Checking whether GPU is avaliable and if it should be used
         if model.device == 'cuda':
@@ -87,9 +68,11 @@ for e in range(fine_tune_epochs):
             x_batch = x_batch.cuda()
             y_batch = y_batch.cuda()
 
-
         # Passing the batch down the model
         y = model(x_batch)
+
+        # Reshaping the outputs
+        y = y.reshape(x_batch.size(0), model.hidden_shape[0] * model.hidden_shape[1] * n_filters)
 
         # Calculating the fully-connected outputs
         y = fc(y)
@@ -110,7 +93,6 @@ for e in range(fine_tune_epochs):
         
     # Calculate the test accuracy for the model:
     for x_batch, y_batch in tqdm(val_batch):
-
         # Checking whether GPU is avaliable and if it should be used
         if model.device == 'cuda':
             # Applies the GPU usage to the data and labels
@@ -119,6 +101,9 @@ for e in range(fine_tune_epochs):
 
         # Passing the batch down the model
         y = model(x_batch)
+
+        # Reshaping the outputs
+        y = y.reshape(x_batch.size(0), model.hidden_shape[0] * model.hidden_shape[1] * n_filters)
 
         # Calculating the fully-connected outputs
         y = fc(y)
@@ -133,3 +118,6 @@ for e in range(fine_tune_epochs):
 
 # Saving the fine-tuned model
 torch.save(model, 'tuned_model.pth')
+
+# Checking the model's history
+print(model.history)
