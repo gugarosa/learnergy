7920d8a51a3dc9fe81af7b22dd6ae7d9fc556f46
Gustavo Rosa <gth.rosa@uol.com.br>
2020-12-02 15:59:40 -0300

    Bumping version for future release.

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,10 @@ copyright = '2020, Mateus Roder and Gustavo de Rosa'
 author = 'Mateus Roder and Gustavo de Rosa'
 
 # The short X.Y version
-version = '1.1.0'
+version = '1.1.1'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
