be72dd9fcf09cacba60348e60c8c00066c0cf2d0
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2020-12-12 10:36:08 -0300

    update dropout_rbm num_workers

--- learnergy/models/bernoulli/dropout_rbm.py
+++ learnergy/models/bernoulli/dropout_rbm.py
@@ -133,7 +133,7 @@ class DropoutRBM(RBM):
         self.p = 0
 
         # Transforming the dataset into testing batches
-        batches = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        batches = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
         # For every batch
         for samples, _ in tqdm(batches):
