f11c9115488268c322bc9991ae9cffb854ef5e5b
Gustavo Rosa <gth.rosa@uol.com.br>
2021-04-01 15:44:53 -0300

    Adding function to save tensor instead of only showing.

--- learnergy/visual/tensor.py
+++ learnergy/visual/tensor.py
@@ -8,6 +8,44 @@ import learnergy.utils.logging as l
 logger = l.get_logger(__name__)
 
 
+def save_tensor(tensor, output_path):
+    """Saves a tensor in grayscale mode using Matplotlib.
+
+    Args:
+        tensor (Tensor): An input tensor to be saved.
+        output_path (str): An outputh path to save the tensor.
+
+    """
+
+    logger.debug('Saving tensor ...')
+
+    # Creates a matplotlib figure
+    plt.figure()
+
+    # Checks if tensor has 3 channels
+    if tensor.size(0) == 3:
+        # If yes, permutes the tensor
+        tensor = tensor.permute(1, 2, 0)
+
+        # Plots without a color map
+        plt.imshow(tensor.cpu().detach().numpy())
+
+    # If the tensor is grayscale
+    else:
+        # Plots the numpy version of the tensor (grayscale)
+        plt.imshow(tensor.cpu().detach().numpy(), cmap=plt.cm.get_cmap('gray'))
+
+    # Disables all axis' ticks
+    plt.xticks([])
+    plt.yticks([])
+
+    # Shows the plot
+    plt.savefig(output_path)
+
+    logger.debug('Tensor saved.')
+
+
+
 def show_tensor(tensor):
     """Plots a tensor in grayscale mode using Matplotlib.
 
