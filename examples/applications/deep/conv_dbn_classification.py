0aaec985d02726edb3ba0c19e5e7df707d4b78cf
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2023-01-21 18:28:27 -0300

    updating conv_dbn example

--- examples/applications/deep/conv_dbn_classification.py
+++ examples/applications/deep/conv_dbn_classification.py
@@ -1,10 +1,10 @@
 import torch
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 from learnergy.models.deep import ConvDBN
 
 # Creating training and testing dataset
@@ -21,47 +21,55 @@ test = torchvision.datasets.MNIST(
     transform=torchvision.transforms.ToTensor(),
 )
 
+vshape = 28
+channels = 1
 # Creating a ConvDBN
 model = ConvDBN(
-    model="gaussian",
-    visible_shape=(28, 28),
-    filter_shape=((5, 5), (5, 5)),
+    model="bernoulli",
+    visible_shape=(vshape, vshape),
+    filter_shape=((3, 3), (5, 5)),
     n_filters=(32, 32),
     steps=(1, 1),
-    n_channels=1,
-    learning_rate=(0.0001, 0.00001),
-    momentum=(0, 0),
+    n_channels=channels,
+    learning_rate=(0.00001, 0.000001),
+    momentum=(0.9, 0.9),
     decay=(0, 0),
-    maxpooling=(True, False),
-    # pooling_kernel=(2, 0), # WORKING ON ...
+    maxpooling=(False, True),
+    #pooling_kernel=(2, 2, 2), # WORKING ON ...
     use_gpu=True,
 )
+# If you want to disable the input (visible) normalization, uncomment the lines (if its necessary, add more models)
+#model.models[0].normalize=False
+#model.models[1].normalize=False
 
 batch_size = 128
 n_classes = 10
-fine_tune_epochs = 10
-epochs = (1, 1)
+fine_tune_epochs = 20
+epochs = (20, 20)
 
 # Training a ConvDBN
 model.fit(train, batch_size=batch_size, epochs=epochs)
 
 # Reconstructing test set
-# rec_mse, v = model.reconstruct(test)
+#rec_mse, v = model.reconstruct(test)
 
 # Saving model
 torch.save(model, "model.pth")
 
-# Creating the Fully Connected layer to append on top of DBN
-h1 = model.models[len(model.models) - 1].hidden_shape[0]
-h2 = model.models[len(model.models) - 1].hidden_shape[1]
-nf = model.models[len(model.models) - 1].n_filters
+# Loading model
+#torch.load('model.pth')
 
-if model.models[len(model.models) - 1].maxpooling:
-    input_fc = nf * (h1 // 2 + 1) * (h2 // 2 + 1)
-    print("pooling", input_fc)
+# Creating the Fully Connected layer to append on top of DBN
+h1 = model.models[len(model.models)-1].hidden_shape[0]
+h2 = model.models[len(model.models)-1].hidden_shape[1]
+nf = model.models[len(model.models)-1].n_filters
+
+if model.models[len(model.models)-1].maxpooling:    
+    input_fc = nf * (h1//2 + 1) * (h2//2 + 1)
+    print('Pooling:', input_fc)
 else:
     input_fc = nf * h1 * h2
-fc = nn.Linear(input_fc, n_classes)
+fc = nn.Linear(input_fc , n_classes)
 
 # Check if model uses GPU
 if model.device == "cuda":
@@ -72,12 +80,12 @@ if model.device == "cuda":
 criterion = nn.CrossEntropyLoss()
 
 # Creating the optimzers
-optimizer = [optim.Adam(m.parameters(), lr=0.0001) for m in model.models]
+optimizer = [optim.Adam(m.parameters(), lr=0.00001) for m in model.models]
 optimizer.append(optim.Adam(fc.parameters(), lr=0.001))
 
 # Creating training and validation batches
 train_batch = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=0)
-val_batch = DataLoader(test, batch_size=10000, shuffle=False, num_workers=0)
+val_batch = DataLoader(test, batch_size=256, shuffle=False, num_workers=0)
 
 # For amount of fine-tuning epochs
 for e in range(fine_tune_epochs):
@@ -101,9 +109,10 @@ for e in range(fine_tune_epochs):
 
         # Passing the batch down the model
         y = model(x_batch)
-
+        
         # Reshaping the outputs
-        y = y.reshape(x_batch.size(0), input_fc)
+        y = y.reshape(
+            x_batch.size(0), input_fc)
 
         # Calculating the fully-connected outputs
         y = fc(y)
@@ -132,9 +141,10 @@ for e in range(fine_tune_epochs):
 
         # Passing the batch down the model
         y = model(x_batch)
-
+        
         # Reshaping the outputs
-        y = y.reshape(x_batch.size(0), input_fc)
+        y = y.reshape(
+            x_batch.size(0), input_fc)
 
         # Calculating the fully-connected outputs
         y = fc(y)
@@ -143,13 +153,13 @@ for e in range(fine_tune_epochs):
         _, preds = torch.max(y, 1)
 
         # Calculating validation set accuracy
-        val_acc = torch.mean((torch.sum(preds == y_batch).float()) / x_batch.size(0))
+        val_acc += torch.mean((torch.sum(preds == y_batch).float()) / x_batch.size(0))
 
-    print(f"Loss: {train_loss / len(train_batch)} | Val Accuracy: {val_acc}")
+    print(f"Loss: {train_loss / len(train_batch)} | Val Accuracy: {val_acc/len(val_batch)}")
 
 # Saving the fine-tuned model
 torch.save(model, "tuned_model.pth")
 
 # Checking the model's history
-for m in model.models:
-    print(m.history)
+#for m in model.models:
+#    print(m.history)
